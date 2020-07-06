6f62391fb70eecd6439e4e0d26d455313b018720
Lime Peach <limepeach@LimePeach.local>
2020-07-06 01:57:23 -0700

    Updates

---
+++ precache-manifest.eeacc3cad199ed5937ac972d2a4bf205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783f83d8c6bc6eb36464623ec9d4f555",
+    "url": "/dog-api/index.html"
+  },
+  {
+    "revision": "5a8bc347c71c626ace52",
+    "url": "/dog-api/static/css/main.b2f88c5f.chunk.css"
+  },
+  {
+    "revision": "3893c81b895553bee518",
+    "url": "/dog-api/static/js/2.d4d5b479.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dog-api/static/js/2.d4d5b479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8bc347c71c626ace52",
+    "url": "/dog-api/static/js/main.67bc3fd3.chunk.js"
+  },
+  {
+    "revision": "303182bbd590e473b1a6",
+    "url": "/dog-api/static/js/runtime-main.c7c68f1f.js"
+  }
+]);
